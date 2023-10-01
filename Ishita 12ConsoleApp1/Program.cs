e7e84eb5721c35300f819cf311398bc2e3a9becb
A <A@ANUJ>
2023-09-30 21:40:10 -0400

    Add project files.

---
+++ Ishita 12ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_12ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, spc, rows, k;
+
+            Console.Write("\n\n");
+            Console.Write("Display the pattern like pyramid with asterisk:\n");
+            Console.Write("-------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            spc = rows + 4 - 1;
+            for (i = 1; i <= rows; i++)
+            {
+                for (k = spc; k >= 1; k--) ;
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 1; j <= i; j++)
+                    Console.Write("* ");
+                Console.Write("\n");
+                spc--;
+            }
+        }
+    }
+}
